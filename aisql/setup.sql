b3f0a9963b59a8bf47de6ef3351a43a781d5f475
Kaydance Turner <kaydancegillam@gmail.com>
2025-09-27 12:58:55 -0600

    Refactor: change syntax to work with sqllite

--- aisql/setup.sql
+++ aisql/setup.sql
@@ -1,46 +1,46 @@
 CREATE TABLE person (
-    person_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    person_id INTEGER UNSIGNED NOT NULL  PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE musician (
-    person_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+    person_id INTEGER UNSIGNED NOT NULL PRIMARY KEY,
     position VARCHAR(50),
     FOREIGN KEY (person_id) REFERENCES person(person_id)
 );
 
 CREATE TABLE conductor (
-    person_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+    person_id INTEGER UNSIGNED NOT NULL PRIMARY KEY,
     affiliation VARCHAR(100),
     FOREIGN KEY (person_id) REFERENCES person(person_id)
 );
 
 CREATE TABLE instrument (
-    instrument_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    instrument_id INTEGER UNSIGNED NOT NULL  PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     family VARCHAR(50) NOT NULL,
     owned_by_orchestra BOOLEAN NOT NULL
 );
 
 CREATE TABLE instrument_musician (
-    instrument_id BIGINT UNSIGNED NOT NULL,
-    musician_id BIGINT UNSIGNED NOT NULL,
+    instrument_id INTEGER UNSIGNED NOT NULL,
+    musician_id INTEGER UNSIGNED NOT NULL,
     PRIMARY KEY (instrument_id, musician_id),
     FOREIGN KEY (instrument_id) REFERENCES instrument(instrument_id),
     FOREIGN KEY (musician_id) REFERENCES musician(person_id)
 );
 
 CREATE TABLE concert_musician (
-    concert_id BIGINT UNSIGNED NOT NULL,
-    musician_id BIGINT UNSIGNED NOT NULL,
+    concert_id INTEGER UNSIGNED NOT NULL,
+    musician_id INTEGER UNSIGNED NOT NULL,
     PRIMARY KEY (concert_id, musician_id),
     FOREIGN KEY (concert_id) REFERENCES concert(concert_id),
     FOREIGN KEY (musician_id) REFERENCES musician(person_id)
 );
 
 CREATE TABLE address (
-    address_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    address_id INTEGER UNSIGNED NOT NULL  PRIMARY KEY,
     street VARCHAR(255) NOT NULL,
     city VARCHAR(100) NOT NULL,
     state CHAR(2) NOT NULL,
@@ -48,25 +48,26 @@ CREATE TABLE address (
 );
 
 CREATE TABLE concert (
-    concert_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    concert_id INTEGER UNSIGNED NOT NULL  PRIMARY KEY,
     date DATETIME NOT NULL,
     title VARCHAR(255) NOT NULL,
-    address_id BIGINT UNSIGNED NOT NULL,
-    conductor_id BIGINT UNSIGNED NOT NULL,
+    address_id INTEGER UNSIGNED NOT NULL,
+    conductor_id INTEGER UNSIGNED NOT NULL,
     FOREIGN KEY (address_id) REFERENCES address(address_id),
     FOREIGN KEY (conductor_id) REFERENCES conductor(person_id)
 );
 
 CREATE TABLE song (
-    song_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    song_id INTEGER UNSIGNED NOT NULL  PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     composer VARCHAR(255) NOT NULL,
     duration_in_minutes INT NOT NULL
 );
 
 CREATE TABLE song_concert (
-    concert_id BIGINT UNSIGNED NOT NULL,
-    song_id BIGINT UNSIGNED NOT NULL,
+    concert_id INTEGER UNSIGNED NOT NULL,
+    song_id INTEGER UNSIGNED NOT NULL,
+    order_number INTEGER NOT NULL,
     PRIMARY KEY (concert_id, song_id),
     FOREIGN KEY (concert_id) REFERENCES concert(concert_id),
     FOREIGN KEY (song_id) REFERENCES song(song_id)
